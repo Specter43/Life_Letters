99feae4d5dd2a2f8020e9ce6efa46bcc0da7873a
Specter43 <zheyuyang43@gmail.com>
2023-10-23 16:22:55 -0400

    TRANSFER

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { SearchDialog } from '@/components/SearchDialog'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
